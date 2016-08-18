6e712b7b721384842ff7a16a6af73000e60a267b
Benjamin P Toews <mastahyeti@users.noreply.github.com>
2016-08-17 21:12:22 -0600

    do stuff

--- Extension/ActionRequestHandler.swift
+++ Extension/ActionRequestHandler.swift
@@ -22,7 +22,9 @@ class ActionRequestHandler: NSObject, NSExtensionRequestHandling {
             if KeyInterface.generateTouchIDKeyPair(keyName) {
                 print("generated key pair")
             } else {
+                completion(nil, NSError(domain: "com.github.SecurityKey.Extension", code: 1, userInfo: [:]))
                 print("error generating key pair")
+                return
             }
         }
         
@@ -32,32 +34,67 @@ class ActionRequestHandler: NSObject, NSExtensionRequestHandling {
     func beginRequestWithExtensionContext(context: NSExtensionContext) {
         self.extensionContext = context
         
-        guard
-            let inputItem = extensionContext!.inputItems.first as? NSExtensionItem,
-            let itemProvider = inputItem.attachments?.first as? NSItemProvider
-        else {
-            return
+        var found = false
+        
+        outer:
+            for item: AnyObject in context.inputItems {
+                let extItem = item as! NSExtensionItem
+                if let attachments = extItem.attachments {
+                    for itemProvider: AnyObject in attachments {
+                        if itemProvider.hasItemConformingToTypeIdentifier(kUTTypePropertyList as String) {
+                            itemProvider.loadItemForTypeIdentifier(kUTTypePropertyList as String, options: nil, completionHandler: { (item, error) in
+                                let dictionary = item as! NSDictionary
+                                let javaScriptValues = dictionary[NSExtensionJavaScriptPreprocessingResultsKey] as! NSDictionary
+
+                                NSOperationQueue.mainQueue().addOperationWithBlock {
+                                    self.itemLoadCompletedWithPreprocessingResults(javaScriptValues)
+                                }
+                            })
+                            found = true
+                            break outer
+                        }
+                    }
+                }
         }
         
-        
-        itemProvider.loadItemForTypeIdentifier(kUTTypePropertyList as String, options: nil) { (dict, error) in
-            let itemDictionary = dict as! NSDictionary
-            let javaScriptValues = itemDictionary[NSExtensionJavaScriptPreprocessingResultsKey] as! NSDictionary
-            
-            guard
-                let origin = javaScriptValues["origin"] as? String,
-                let message = javaScriptValues["message"] as? String
+        if !found {
+            self.doneWithResults(["error": "failed to find message"])
+        }
+    }
+    
+    func itemLoadCompletedWithPreprocessingResults(javaScriptValues: NSDictionary) {
+        guard
+            let origin = javaScriptValues["origin"] as? String,
+            let message = javaScriptValues["message"] as? String
             else {
                 return
-            }
-            
-            self.signMessage(origin, message) { (sig, err) in
-                if err == nil {
-                    print(sig)   
-                }
-            }
         }
         
-        self.extensionContext!.completeRequestReturningItems([], completionHandler: nil)
+        self.signMessage(origin, message) { (sig, err) in
+            if err == nil {
+                let strSig = String(data: sig, encoding: NSASCIIStringEncoding)!
+                let key = KeyInterface.publicKeyBits(origin)
+                let strKey = String(data: key, encoding: NSASCIIStringEncoding)!
+
+                self.doneWithResults(["signature": strSig, "key": strKey])
+            } else {
+                self.doneWithResults(["error": "failed to sign message"])
+            }
+        }
+    }
+    
+    func doneWithResults(resultsForJavaScriptFinalizeArg: [NSObject: AnyObject]?) {
+        if let resultsForJavaScriptFinalize = resultsForJavaScriptFinalizeArg {
+            let resultsDictionary = [NSExtensionJavaScriptFinalizeArgumentKey: resultsForJavaScriptFinalize]
+            let resultsProvider = NSItemProvider(item: resultsDictionary, typeIdentifier: String(kUTTypePropertyList))
+            let resultsItem = NSExtensionItem()
+            resultsItem.attachments = [resultsProvider]
+            
+            self.extensionContext!.completeRequestReturningItems([resultsItem], completionHandler: nil)
+        } else {
+            self.extensionContext!.completeRequestReturningItems([], completionHandler: nil)
+        }
+        
+        self.extensionContext = nil
     }
 }
