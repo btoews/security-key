5de5db1e24bc562a045b4200bcdf83c7a694f53d
Benjamin P Toews <mastahyeti@users.noreply.github.com>
2016-08-22 09:15:38 -0600

    remove empty file

--- SelfSignedCertificate.m
+++
@@ -1,9 +0,0 @@
-//
-//  SelfSignedCertificate.m
-//  SecurityKey
-//
-//  Created by Benjamin P Toews on 8/19/16.
-//  Copyright © 2016 mastahyeti. All rights reserved.
-//
-
-#import <Foundation/Foundation.h>
