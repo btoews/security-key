fa35a23e8e4aa025bb7f446e0cc21b8d222c4fec
Keith Cirkel <keithamus@users.noreply.github.com>
2018-02-06 11:39:03 +0000

    upgrade global function to better detect global
    This fixes the global detection in environments which are implicitly strict - where `this` is not the global object.

--- u2f_polyfill.js
+++ u2f_polyfill.js
@@ -57,5 +57,13 @@
     }
     global[""] = exports;
 })({}, function() {
-    return this;
+    if (typeof self !== 'undefined') {
+        return self;
+    } else if (typeof window !== 'undefined') {
+        return window;
+    } else if (typeof global !== 'undefined') {
+        return global;
+    } else {
+        return Function('return this')();
+    }
 }());
